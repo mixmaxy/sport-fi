9fdd83b416cc1a938894c1b5c69af4f15ec3de63
marrhasiholan <refidaamelia89@gmail.com>
2026-06-04 18:01:05 +0700

    feat: scaffold initial sport reservation project structure and core features

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "placehold.co" },
+      { protocol: "https", hostname: "**.dibimbing.id" },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+    ],
+  },
 };
 
 export default nextConfig;
